db84e6a1fa2720e972b797bdc61ab39b04f7e7dd
graceyu12 <gyu12@terpmail.umd.edu>
2025-05-16 10:57:58 -0400

    import all necessary features

--- actor_clustering_analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import json
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-with open('imdb_movies_2000to2022.prolific.json', 'r') as f:
-    imdb_data = [json.loads(line) for line in f]
