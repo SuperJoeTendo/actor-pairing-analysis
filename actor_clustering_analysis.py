c6d384deebcaa244a74a9de0b7b98bdff6c5a17d
graceyu12 <gyu12@terpmail.umd.edu>
2025-05-16 10:49:08 -0400

    Read the imdb file

--- actor_clustering_analysis.py
+++ actor_clustering_analysis.py
@@ -5,3 +5,5 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+with open('imdb_movies_2000to2022.prolific.json', 'r') as f:
+    imdb_data = [json.loads(line) for line in f]
